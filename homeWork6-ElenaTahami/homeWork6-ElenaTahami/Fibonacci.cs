57eb825f286e5468092967b3737ce7e9bb42389b
elena <HP@LAPTOP-GORF2L20>
2026-08-01 23:09:43 +0330

    HomeWork6-Algorithm-ElenaTahami

---
+++ homeWork6-ElenaTahami/homeWork6-ElenaTahami/Fibonacci.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homeWork6_ElenaTahami
+{
+    class Fibonacci
+    {
+
+        public int Calculate(int n)
+        {
+
+            int firstNumber=0;
+         int secondNumber = 1;
+        int newNumber;
+
+            if (n < 0)
+            {
+                throw new ArgumentException("Number cannot be negative", nameof(n));
+            }
+
+
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            else if (n == 1)
+
+            {
+                return 1;
+
+            }
+
+
+
+            else
+            {
+
+                for (int i = 2; i <= n; i++)
+                {
+
+
+                    newNumber = firstNumber + secondNumber;
+                   firstNumber = secondNumber;
+                    secondNumber = newNumber;
+
+
+                }
+
+                return secondNumber;
+            }
+        }
+    }
+}
---
+++ homeWork6-ElenaTahami/homeWork6-ElenaTahami/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homeWork6_ElenaTahami
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int n=Convert.ToInt32(Console.ReadLine());
+            Fibonacci fibonacci = new Fibonacci();
+          Console.WriteLine( fibonacci.Calculate(n));
+              
+        }
+
+
+    }
+
+}
