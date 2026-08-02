7931278b6154dffa0f0140bd08e5d06d4cc359e7
elena <HP@LAPTOP-GORF2L20>
2026-08-02 09:58:49 +0330

    prime validator

---
+++ homeWork6-ElenaTahami/homeWork6-ElenaTahami/PrimeValidator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homeWork6_ElenaTahami
+{
+    public class PrimeValidator
+    {
+
+        public bool CheckerPrime(int inputnum)
+
+        {
+            //if (inputnum < 2)
+            //{
+
+            //    return " This Number is a not Prime number";
+            //}         
+
+            int sqrtNum = (int)Math.Sqrt(inputnum);
+
+            for (int i=2;i<=sqrtNum ;i++ )
+            {
+                int remainder = inputnum % i;
+
+             if(remainder==0)
+                {
+
+                    return false;
+                }   
+             
+
+
+
+            }
+
+            return true;
+
+        }
+
+          
+            
+    }
+}
--- homeWork6-ElenaTahami/homeWork6-ElenaTahami/Program.cs
+++ homeWork6-ElenaTahami/homeWork6-ElenaTahami/Program.cs
@@ -8,10 +8,25 @@ namespace homeWork6_ElenaTahami
     {
         public static void Main()
         {
+
+
+
+            ////Calculate Fibonacci
             int n=Convert.ToInt32(Console.ReadLine());
             Fibonacci fibonacci = new Fibonacci();
           Console.WriteLine( fibonacci.Calculate(n));
-              
+
+
+         ////checkerPrime
+         
+            int inputNum=Convert.ToInt32(Console.ReadLine())  ;
+            PrimeValidator prime=new PrimeValidator();
+            Console.WriteLine(prime.CheckerPrime(inputNum));
+
+
+
+
+
         }
 
 
